4e96cdba88aaeef480927432ff73716190adddac
hendrikusR <hendrikus.richardo@gmail.com>
2017-02-13 16:55:39 +0700

    add point marker

--- views/site/index.php
+++ views/site/index.php
@@ -14,86 +14,105 @@ $this->title = 'My Yii Application';
             "library-js" => "openlayers",
             "setView"=> "-2, 117",
             "setZoom"=> "5",
-            // "height" => "40%",
+            "height" => "450px",
             "width" => "100%",
-            'draw'  => TRUE
+            //'draw'  => TRUE
         ]
     ]);?>
-
-    <label>Geometry type &nbsp;</label>
-    <select id="type">
-        <option value="Point">Point</option>
-        <option value="LineString">LineString</option>
-        <option value="Polygon">Polygon</option>
-        <option value="Circle">Circle</option>
-        <option value="None">None</option>
-    </select>
 
 
     </div>
 </div>
 <?php
-$script = '
+$script = "
 
-
-var view = map.getView();
-
-
-    var raster = new ol.layer.Tile({
-        source: new ol.source.OSM()
+    var image = new ol.style.Circle({
+        radius: 5,
+        fill: null,
+        stroke: new ol.style.Stroke({color: 'red', width: 1})
     });
 
-    var source = new ol.source.Vector({wrapX: false});
+    var styles = {
+        'Point': new ol.style.Style({
+            image: image,
+            stroke: new ol.style.Stroke({
+                color: 'green',
+                width: 1
+            })
+        })
+    };
 
-    var vector = new ol.layer.Vector({
-        source: source
+    var styleFunction = function(feature) {
+        return styles[feature.getGeometry().getType()];
+    };
+
+    var geojsonObject = {
+        'type': 'FeatureCollection',
+        'crs': {
+            'type': 'name',
+            'properties': {
+                'name': 'EPSG:3857'
+            }
+        },
+        'features': [{
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [106.8310546875, -6.18424616128059]
+            }
+        }]
+    };
+
+    var vectorSource = new ol.source.Vector({
+        features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
     });
+
+    vectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
+
+    var vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: styleFunction
+    });
+
+
+    var view = map.getView();
+
 
     map.addControl(new bukapeta.ol.BasemapProvider({
-        default : ["mapbox","streets-basic"],
+        default : ['mapbox','streets-basic'],
         apikey : {
-            mapbox : "pk.eyJ1IjoiLWhhYmliLSIsImEiOiJjaWdjbmpsZzE0MXM3dmptM3NzN292NWVhIn0.AfZ7s3jnuqK-2nPzbfl7IA",
+            mapbox : 'pk.eyJ1IjoiLWhhYmliLSIsImEiOiJjaWdjbmpsZzE0MXM3dmptM3NzN292NWVhIn0.AfZ7s3jnuqK-2nPzbfl7IA',
         },
-        preset : ["all"]
+        preset : ['all']
     }));
 
-    mapModal = new ol.Map({
-    //target: "mapModal",
-    layers: [raster, vector],
+    map = new ol.Map({
+
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
-            })
+            }),
+            vectorLayer
         ],
-
+        controls: ol.control.defaults({
+            attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+                collapsible: false
+            })
+        }),
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        }),
+        view: new ol.View({
+            projection: ol.proj.get('EPSG:3857'),
+            center: ol.proj.transform([117, -2], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 4,
+        })
     });
 
-    var typeSelect = document.getElementById("type");
-
-    var draw;
-
-    function addInteraction() {
-        var value = typeSelect.value;
-        
-        if(value !== "None") {
-            draw = new ol.interaction.Draw({
-                source : source,
-                type : (typeSelect.value)
-            });
-            map.addInteraction(draw);
-        }
-    }
-
-    typeSelect.onchange = function() {
-        map.removeInteraction(draw);
-        addInteraction();
-    };
-
-    addInteraction();
 
 
 
-
-';
+";
 $this->registerJsFile('/web/js/bukapeta-ol.js',[/*'depends'=> ['\yii\web\JqueryAsset'],*/'position'=>\yii\web\View::POS_END]);
 $this->registerJs($script, View::POS_END)?>
